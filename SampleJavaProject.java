f3b4ed57d8fa62be08e122ac2f8b2de05ae21a0d
TestRuchir <73190210+TestRuchir@users.noreply.github.com>
2020-10-20 23:28:27 +0530

    Update SampleJavaProject.java

--- SampleJavaProject.java
+++ SampleJavaProject.java
@@ -14,7 +14,12 @@ import lombok.Setter;
 @AllArgsConstructor
 public class SampleJavaProject implements Runnable {
 
+int target = -5;
+int num = 3;
 
+target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+target =+ num; // Noncompliant; target = 3
+    
     private static final long PRINT_DELAY = 1000L;
 
 
