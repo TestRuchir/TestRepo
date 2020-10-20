0749eeccb40bdf3b0760aaab75b50f7814e218bb
TestRuchir <73190210+TestRuchir@users.noreply.github.com>
2020-10-20 22:58:56 +0530

    Update SampleJavaProject.java

--- SampleJavaProject.java
+++ SampleJavaProject.java
@@ -9,38 +9,57 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
 
-/**
- * The main class of the application. It contains the main() method,
- * the first method called.
- */
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class SampleJavaProject implements Runnable {
 
-    /** The delay between printed messages. */
+
     private static final long PRINT_DELAY = 1000L;
 
-    /** The name to be printed in the output message. */
+
     @Getter @Setter @NonNull
     @Parameter(names = "--name", description = "set the user's name",
                required = true)
     private String name = "world";
     private String test;
+    private username = "abc";
+    private password = "xyz;
 
-    /** Command line parameter for --loop. */
+ 
     @Parameter(names = "--loop", description = "print endlessly, hotswap demo")
     private boolean loop = false;
 
-    /** Command line parameter for --help. */
+    
     @Parameter(names = { "-h", "--help" }, description = "print help message")
     private boolean help = false;
 
-    /**
-     * Print the "Hello, world!" string.
-     * @param args application input arguments
-     */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
     public static void main(final String[] args) {
-        /* Parse command line arguments. */
+       
+        
+        
+        
+        
+        
         SampleJavaProject sjp = new SampleJavaProject();
         try {
             JCommander jc = new JCommander(sjp, args);
@@ -57,9 +76,7 @@ public class SampleJavaProject implements Runnable {
         sjp.run();
     }
 
-    /**
-     * Print the "Hello, world!" string.
-     */
+
     public final void sayHello() {
         System.out.printf("Hello, %s!%n", name);
     }
